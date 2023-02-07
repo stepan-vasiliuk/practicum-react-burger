19877d043f92363d351a7a1dcba3653eadeb5a29
stepan_vasiliuk <stepupka@gmail.com>
2023-02-07 15:57:09 +0300

    header-markUp

---
+++ src/components/app/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import appStyles from './app.module.css';
+import Header from "../header/header";
+
+class App extends React.Component {
+    render() {
+
+        return (
+            <>
+                <Header/>
+                <main>
+
+                </main>
+            </>
+        )
+
+    }
+
+}
+
+export default App;
